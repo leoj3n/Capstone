b7e383e5d77caab463d4ea055873e6711c644313
Joel Kuczmarski <leoj3n@gmail.com>
2012-02-08 09:25:03 -0600

    initial commit

---
+++ UnityProject/Assets/Custom Assets/Scripts/Play.js
@@ -0,0 +1,15 @@
+
+function Start() {
+	renderer.material.mainTexture.Play();
+	renderer.material.GetTexture( '_Mask' ).Play();
+}
+
+/*
+function Start() {
+	renderer.material.mainTexture.Play();
+}
+
+function Update() {
+	// renderer.material.mainTexture = '...'
+}
+*/
