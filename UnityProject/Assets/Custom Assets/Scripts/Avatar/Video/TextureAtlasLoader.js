9ea916e44d3ae8985070ec5f09fd556e530fcba8
unknown <jkuczmar@634LAB03.cstcis.cti.depaul.edu>
2012-04-23 21:10:27 -0500

    improved texture atlas + meteor prefab?

--- UnityProject/Assets/Custom Assets/Scripts/Avatar/Video/TextureAtlasLoader.js
+++ UnityProject/Assets/Custom Assets/Scripts/Avatar/Video/TextureAtlasLoader.js
@@ -24,10 +24,12 @@ function Awake() {
 	origScale = transform.localScale;
 	scaleFactor = (renderer.material.mainTexture.width / origScale.x);
 	
+	frames = new ArrayList();
 	loadFrames( texture[0], atlas[0] );
 }
 
 function Update() {
+	// Debug: toggle textures
 	if( Input.GetKeyDown( KeyCode.Space ) ) {
 		switch( type ) {
 			case atlasType.binary:
@@ -43,8 +45,8 @@ function Update() {
 }
 
 function loadFrames( tex : Texture2D, data : TextAsset ) {
+	// detect type of data
 	var path = AssetDatabase.GetAssetPath( data );
-	
 	if (path.Contains( '.bytes' ))
 		type = atlasType.binary;
 	else if (path.Contains( '.txt' ))
@@ -52,46 +54,26 @@ function loadFrames( tex : Texture2D, data : TextAsset ) {
 	else
 		type = atlasType.none;
 	
+	frames.Clear();
+	
 	switch( type ) {
 		case atlasType.binary:
-			var s : System.IO.Stream = new System.IO.MemoryStream( data.bytes );
-			var br : System.IO.BinaryReader = new System.IO.BinaryReader( s );
+			var br = System.IO.BinaryReader( System.IO.MemoryStream( data.bytes ) );
 			width = read4Bytes( br );
 			height = read4Bytes( br );
 			var numFrames = read4Bytes( br );
-			var rects = new Rect[numFrames];
 			
-			// get rectangles
 			for( var i = 0; i < numFrames; i++ )
-				rects[i] = Rect( read4Bytes( br ), read4Bytes( br ), read4Bytes( br ), read4Bytes( br ) );
-			
-			// skip names
-			for (i = 0; i < numFrames; i++)
-				while (br.ReadChar() != 0);
-			
-			frames = new ArrayList(); // will be an array of rects
-			
-			// set rectangle offsets
-			for( i = 0; i < numFrames; i++ ) {
-				var left = read4Bytes( br );
-				var top = read4Bytes( br );
-				var right = read4Bytes( br );
-				var bottom = read4Bytes( br );
-				
-				var rectAndOffset = new ArrayList();
-				rectAndOffset.Add( rects[i] );
-				rectAndOffset.Add( RectOffset( left, right, top, bottom ) );
-				frames.Add( rectAndOffset );
-			}
+				frames.Add( Rect( read4Bytes( br ), read4Bytes( br ), read4Bytes( br ), read4Bytes( br ) ) );
 			break;
 		case atlasType.json:
 			var dict = Json.Deserialize( data.text ) as System.Collections.Generic.Dictionary.<String, Object>;
 			
-			frames = new ArrayList(); // will be an array of dictionary keys/pairs
-			for (var frame in dict['frames'].Values) frames.Add( frame ); //Debug.Log( frames[0]['frame']['x'] );
-			
 			width = dict['meta']['size']['w'];
-			height = dict['meta']['size']['h'];			
+			height = dict['meta']['size']['h'];
+			
+			for (var frame in dict['frames'].Values)
+				frames.Add( Rect( frame['frame']['x'], frame['frame']['y'], frame['frame']['w'], frame['frame']['h'] ) ); 
 			break;
 	}
 	
@@ -99,38 +81,48 @@ function loadFrames( tex : Texture2D, data : TextAsset ) {
 }
 
 function renderFrame() {
+	Debug.Log( frames.Count );
+	
 	var index = parseInt( (Time.timeSinceLevelLoad * fps) % (frames.Count) );
-	var frame : Rect;
-	var trimmed : boolean = false;
-	
-	switch( type ) {
-		case atlasType.binary:
-			var r = frames[index];
-			trimmed = ((r[1].horizontal + r[1].vertical) > 0);
-			//frame = (trimmed ? r[1].Remove( r[0] ) : r[0]);
-			//if (trimmed)
-			//	frame = Rect( r[0].x + r[1].left, r[0].y + r[1].top, r[0].width - r[1].right, r[0].height - r[1].bottom ); 
-			//else
-				frame = r[0];
+	var frame : Rect = frames[index];
 				
-			//Debug.Log( r[0] + ' ... ' + r[1] + ' ... ' + frame );
-			
-			transform.localPosition = Vector3.zero; // hack
-			break;
-		case atlasType.json:			
-			var f = frames[index]['frame'];
-			frame = Rect( f['x'], f['y'], f['w'], f['h'] );
-			trimmed = frames[index]['trimmed'];
-			
-			transform.localPosition = Vector3( -0.723588, 0.25, 0.0 ); // hack
-			break;
-	}
-	
 	renderer.material.mainTextureOffset = Vector2( (frame.x / width), 1.0 - ((frame.y + frame.height) / height) );
 	renderer.material.mainTextureScale = Vector2( (frame.width / width), (frame.height / height) );
-	transform.localScale = (trimmed ? Vector3( (frame.width / scaleFactor), (frame.height / scaleFactor), transform.localScale.z ) : origScale );
+	transform.localScale = Vector3( (frame.width / scaleFactor), (frame.height / scaleFactor), transform.localScale.z );
 }
 
 function read4Bytes( reader : System.IO.BinaryReader ) {
 	return System.BitConverter.ToInt32( reader.ReadBytes( 4 ), 0 );
 }
+
+/*
+
+TexturePacker Settings:
+	
+	Data Format: Unity3D
+	Texture Format: PNG
+	Image Format: RGBA8888
+	Dithering: NearestNeighbor
+	Autosize: True
+	Max Width: 4096
+	Max Height: 4096
+	Algorithm: MaxRects
+	Heuristics: Best
+	Border Padding: 0
+	Shape Padding: 0
+	Inner Padding: 0
+	Extrude: 0
+	Trim: True
+	Trim/Crop Threshold: True
+	Enable Auto Alias: True
+	
+
+Texture Atlas Generator Settings:
+	
+	Pack Transparent: True
+	Max Width: 4096
+	Power of 2 Width: True
+	Power of 2 Height: True
+	Output Formats: PNG, TXA
+	
+*/
